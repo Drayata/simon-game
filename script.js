27242e5a5e91bc2c4de02bb192fd80c42c1f2586
drayata <profirstindra@gmail.com>
2026-07-15 22:39:40 +0700

    add sound effect

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-document.querySelectorAll(".box").forEach(function (box) {
-  box.addEventListener("click", function () {
-    alert("Box clicked!");
+const boxes = document.querySelectorAll(".box");
+const boxList = ["red", "yellow", "green", "blue"];
+let gamePattern = [];
+let userPattern = [];
+let isGameStarted = false;
+
+//FUNCTIONS
+
+const addGamePattern = () => {
+  gamePattern.push(boxList[Math.floor(Math.random() * 4 + 1)]);
+};
+
+const answerCheck = (box) => {
+  if (userPattern !== gamePattern) {
+    wait;
+  }
+};
+
+const clickedSound = (box) => {
+  const boxSound = new Audio(`./sound/${box}.mp3`);
+  boxSound.play();
+};
+
+//MAIN
+document.querySelector("body").addEventListener("keydown", () => {});
+
+boxes.forEach((box) => {
+  box.addEventListener("click", (event) => {
+    console.log(event);
+    clickedSound(event.target.id);
   });
 });
